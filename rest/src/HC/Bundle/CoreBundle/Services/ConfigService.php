11150e7694515224f3dbbbbe60048b0e819efd7e
Shawn Williams <shawnbeta@outlook.com>
2015-10-22 11:00:20 -0700

    Saving

--- rest/src/HC/Bundle/CoreBundle/Services/ConfigService.php
+++ rest/src/HC/Bundle/CoreBundle/Services/ConfigService.php
@@ -16,7 +16,11 @@ class ConfigService
 
     const TEST_ITEM_COUNT = 20;
 
+    const EPISODE_DAY_MAX = 60;
+
     const DEV_SERVER_PATH = '78.74.14.2';
+
+    const ADMIN_KEY = 'AIzaSyAIV2ia1Wor';
 
 
 }
--- rest/src/HC/Bundle/MediaBundle/Services/EpisodeService.php
+++ rest/src/HC/Bundle/MediaBundle/Services/EpisodeService.php
@@ -3,7 +3,9 @@ namespace HC\Bundle\MediaBundle\Services;
 use HC\Bundle\MediaBundle\Entity\Episode;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
+use HC\Bundle\MediaBundle\Services\Parsers\SimplePieService;
+use HC\Bundle\MediaBundle\Services\Parsers\ZetaXMLService;
+use HC\Bundle\UtilityBundle\Services\UtilityService;
 
 class EpisodeService extends MediaService
 {
@@ -18,6 +20,7 @@ class EpisodeService extends MediaService
 		$Episode->setPubDate($data['pubDate']);
 		$Episode->setWatched(false);
 		$Episode->setBookmark(0.0);
+		$Episode->setMediaType($subscription->getMediaType());
 		$Episode->setSubscription($subscription);
 		// Use the current date/time for create and modified dates
 		$dateAgo = new \DateTime;
@@ -43,7 +46,7 @@ class EpisodeService extends MediaService
 				$em->flush();
 				//$em->clear(); // Detaches all objects from Doctrine!
 			}
-			array_push($episodeCollection, $this->getSerializer()->serialize($Episode, 'json'));
+			array_push($episodeCollection, $this->getEpisodeArray($Episode));
 		}
 		$em->flush(); //Persist objects that did not make up an entire batch
 		$em->clear();
@@ -91,8 +94,52 @@ class EpisodeService extends MediaService
 			'public' => $episode->getPublic(),
 			'pubDate' => $episode->getPubDate(),
 			'watched' => $episode->getWatched(),
-			'bookmark' => $episode->getBookmark()
+			'bookmark' => $episode->getBookmark(),
+			'mediaType' => $episode->getMediaType(),
+			'subscription' => $episode->getSubscription()->getId()
 		);
+	}
+
+	public function deleteOldEpisodes($em, $dateToCompare)
+	{
+		return $em->deleteOldEpisodes('pub_date', $dateToCompare, '<');
+	}
+
+	public function gatherNewEpisodes($sr, $em)
+	{
+
+		// Collect all public subscriptions
+		$subscriptions = $sr->findByPublic(true);
+		$configService = $this->getConfigService();
+
+		$count = count($subscriptions);
+		for($i=0;$i<$count;$i++){
+			// Get data with SimplePie first
+			$simplepie = new SimplePieService($subscriptions[$i]->getSrc());
+			$episodes = $simplepie->getEpisodeCollection($configService::EPISODE_DAY_MAX);
+		}
+
+
+		//Check for empty fields
+		// If the subscription array has any empty values try the ZetaService
+		// Then fold the missing values together
+		if(in_array("", $episodes)){
+			$configService = $this->getConfigService();
+			$dateAgo = $this->getUtilityService()->getDateAgo($configService::EPISODE_DAY_MAX);
+			$subscriptionService = new SubscriptionService();
+			$rsp = $subscriptionService->checkForEmptyFields( new ZetaXMLService(), null, $episodes, $dateAgo);
+			$subscription = $rsp['subscription'];
+			$episodes = $rsp['episodes'];
+		}
+
+		$subscription = $this->createSubscriptionEntity(new Subscription(), $subscription, $src, $episodes[0]['src']);
+		$em->persist($subscription);
+		$em->flush($subscription);
+
+		$episodeService = new EpisodeService();
+		$episodes = $episodeService->buildBulkEpisodes($em, $episodes, $subscription);
+		$rsp = array('subscription' => $this->getSubscriptionArray($subscription), 'episodes' => $episodes);
+
 	}
 
 }
